2e6030d358b5d193b7d6cfd0868665d798dd8d14
shoaibkhalid <shoaibkhalid1996@gmail.com>
2019-05-15 09:59:07 +0500

    Added faq component

---
+++ client/src/app/components/faq/Faq.tsx
@@ -0,0 +1,197 @@
+import * as React from "react";
+import { Card, Row, Col, Collapse } from "antd";
+import { BaseComponent } from "../base/BaseComponent";
+import "./Faq.less";
+import { Link } from "react-router-dom";
+
+const Panel = Collapse.Panel;
+
+export default class Faq extends BaseComponent {
+  render() {
+    const {
+      FAQ,
+      Getting,
+      Started,
+      GSQuestion1,
+      GSQuestion2,
+      GSQuestion3,
+      GSAnswer1,
+      GSAnswer2,
+      GSAnswer3,
+      Account,
+      Managment,
+      AMQuestion1,
+      AMQuestion2,
+      AMQuestion3,
+      AMAnswer1,
+      AMAnswer2,
+      AMAnswer3,
+      Trading,
+      TradeQuestion1,
+      TradeQuestion2,
+      TradeQuestion3,
+      TradeAnswer1,
+      TradeAnswer2,
+      TradeAnswer3,
+      Fees,
+      FeesQuestion1,
+      FeesQuestion2,
+      FeesQuestion3,
+      FeesAnswer1,
+      FeesAnswer2,
+      FeesAnswer3,
+      Security,
+      SecurityQuestion1,
+      SecurityQuestion2,
+      SecurityQuestion3,
+      SecurityAnswer1,
+      SecurityAnswer2,
+      SecurityAnswer3,
+      Payment,
+      PayQuestion1,
+      PayQuestion2,
+      PayQuestion3,
+      PayAnswer1,
+      PayAnswer2,
+      PayAnswer3
+    } = this.lang;
+
+    return (
+      <Card className="faq-card">
+        <h1 className="faq-heading heading-big gx-text-center">
+          {FAQ}
+          <img src="/assets/images/help.svg" alt="FAQ" />
+        </h1>
+        <Row className="row">
+          {/******************************COLUMN #1*************************************/}
+          <Col span={12} className="col-1">
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>
+                {Getting} {Started}
+              </h2>
+              <Panel header={GSQuestion1} key="1">
+                <p>{GSAnswer1}</p>
+              </Panel>
+              <Panel header={GSQuestion2} key="2">
+                <p>
+                  {GSAnswer2} &nbsp;
+                  <Link to={this.getLink(this.constants.RoutePaths.SignUp)}>
+                    here
+                  </Link>
+                </p>
+              </Panel>
+              <Panel header={GSQuestion3} key="3">
+                <p>{GSAnswer3}</p>
+              </Panel>
+            </Collapse>
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>{Trading}</h2>
+              <Panel header={TradeQuestion1} key="1">
+                <p>{TradeAnswer1}</p>
+              </Panel>
+              <Panel header={TradeQuestion2} key="2">
+                <p>{TradeAnswer2}</p>
+              </Panel>
+              <Panel header={TradeQuestion3} key="3">
+                <p>{TradeAnswer3}</p>
+              </Panel>
+            </Collapse>
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>{Security}</h2>
+              <Panel header={SecurityQuestion1} key="1">
+                <p>{SecurityAnswer1}</p>
+              </Panel>
+              <Panel header={SecurityQuestion2} key="2">
+                <p>{SecurityAnswer2}</p>
+              </Panel>
+              <Panel header={SecurityQuestion3} key="3">
+                <p>{SecurityAnswer3}</p>
+              </Panel>
+            </Collapse>
+          </Col>
+
+          {/************************COLUMN #2****************************************/}
+          <Col span={12} className="col-2">
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>
+                {Account} {Managment}
+              </h2>
+              <Panel header={AMQuestion1} key="1">
+                <p>
+                  {AMAnswer1}&nbsp;
+                  <Link to={this.getLink(this.constants.RoutePaths.SignUp)}>
+                    here
+                  </Link>
+                </p>
+              </Panel>
+              <Panel header={AMQuestion2} key="2">
+                <p>{AMAnswer2}</p>
+              </Panel>
+              <Panel header={AMQuestion3} key="3">
+                <p>{AMAnswer3}</p>
+              </Panel>
+            </Collapse>
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>{Fees}</h2>
+              <Panel header={FeesQuestion1} key="1">
+                <p>
+                  {FeesAnswer1}&nbsp;
+                  <Link to={this.getLink(this.constants.RoutePaths.OurFees)}>
+                    {this.lang.Here}
+                  </Link>
+                </p>
+              </Panel>
+              <Panel header={FeesQuestion2} key="2">
+                <p>
+                  {FeesAnswer2}&nbsp;
+                  <Link to={this.getLink(this.constants.RoutePaths.OurFees)}>
+                    {this.lang.Here}
+                  </Link>
+                </p>
+              </Panel>
+              <Panel header={FeesQuestion3} key="3">
+                <p>{FeesAnswer3}</p>
+              </Panel>
+            </Collapse>
+            <Collapse
+              bordered={false}
+              defaultActiveKey={["-1"]}
+              className="collapse text-medium"
+            >
+              <h2>{Payment}</h2>
+              <Panel header={PayQuestion1} key="1">
+                <p>{PayAnswer1}</p>
+              </Panel>
+              <Panel header={PayQuestion2} key="2">
+                <p>{PayAnswer2}</p>
+              </Panel>
+              <Panel header={PayQuestion3} key="3">
+                <p>{PayAnswer3}</p>
+              </Panel>
+            </Collapse>
+          </Col>
+        </Row>
+      </Card>
+    );
+  }
+}
